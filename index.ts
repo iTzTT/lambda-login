edbe1bbf534f3260be735794171da1d38e577430
itztt <itztt23@gmail.com>
2022-04-12 15:56:15 -0600

    adding initial files

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from "aws-lambda";
+import * as _ from 'lodash';
+
+export const handler: Handler = async (event: APIGatewayProxyEvent) : Promise<APIGatewayProxyResultV2> => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
