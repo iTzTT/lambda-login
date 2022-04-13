f7068cf2652a565cae3e35bf37465028bd66dbfe
itztt <itztt23@gmail.com>
2022-04-12 21:45:21 -0600

    fix user

--- index.ts
+++ index.ts
@@ -25,7 +25,7 @@ const knex = require('knex')({
 
 export const handler: Handler = async (event: APIGatewayProxyEvent) : Promise<APIGatewayProxyResultV2> => {   
     try {
-        await knex.select('UserId').from('dbo.User');
+        await knex.select('UserId').from('User');
         const response = {
             statusCode: 200,
             body: JSON.stringify('Hello from Lambda 23232323!'),
