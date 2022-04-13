f22853c351500586f3b08eb4f33398fffd2aebbe
itztt <itztt23@gmail.com>
2022-04-12 21:49:00 -0600

    testing knex as a whole

--- index.ts
+++ index.ts
@@ -25,7 +25,14 @@ const knex = require('knex')({
 
 export const handler: Handler = async (event: APIGatewayProxyEvent) : Promise<APIGatewayProxyResultV2> => {   
     try {
-        await knex.select('UserId').from('User');
+        // await knex.select('UserId').from('User');
+
+        knex.raw("SELECT VERSION()").then(
+            (version) => console.log((version[0][0]))
+        ).catch((err) => { console.log( err); throw err })
+            .finally(() => {
+                knex.destroy();
+            });
         const response = {
             statusCode: 200,
             body: JSON.stringify('Hello from Lambda 23232323!'),
