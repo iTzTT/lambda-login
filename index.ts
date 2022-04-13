27c13db9cdf29069e86629765762823c656108ff
itztt <itztt23@gmail.com>
2022-04-12 21:33:09 -0600

    test deployment

--- index.ts
+++ index.ts
@@ -1,31 +1,31 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from "aws-lambda";
 import * as _ from 'lodash';
 import * as AWS from 'aws-sdk';
-import { Knex } from 'knex';
-AWS.config.update({region: 'us-east-1'});
+// import { Knex } from 'knex';
+// AWS.config.update({region: 'us-east-1'});
 
-const host = 'welldb.cmrswdjikbhu.us-east-1.rds.amazonaws.com';
-const user = 'admin';
-const password = 'wellplayed';
-const database = 'welldb';
+// const host = 'welldb.cmrswdjikbhu.us-east-1.rds.amazonaws.com';
+// const user = 'admin';
+// const password = 'wellplayed';
+// const database = 'welldb';
 
-const connection = {
-    ssl: { rejectUnauthorized: false },
-    host,
-    user,
-    password,
-    database
-}
+// const connection = {
+//     ssl: { rejectUnauthorized: false },
+//     host,
+//     user,
+//     password,
+//     database
+// }
 
-const knex = require('knex')({
-    client: 'mysql',
-    connection
-  });
+// const knex = require('knex')({
+//     client: 'mysql',
+//     connection
+//   });
 
 
 export const handler: Handler = async (event: APIGatewayProxyEvent) : Promise<APIGatewayProxyResultV2> => {   
     try {
-        await knex.select().from('dbo.User');
+        // await knex.select().from('dbo.User');
         const response = {
             statusCode: 200,
             body: JSON.stringify('Hello from Lambda!'),
